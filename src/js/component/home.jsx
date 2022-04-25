c68d5417ac5a6988805b621dfe41535765b69e90
abicelaya <abicelaya@gmail.com>
2022-04-25 15:44:26 +0000

    list terminada

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 
 //create your first component
 const List = () => {
+	const [items, setItems] = useState([]);
 	return (
-		<div className="container d-flex justify-content-center">
+		<div className="container-fluid justify-content-center">
 			<div className="row">
-				<div className="col-12 mt-5">Tareas por hacer</div>
+				<div className="titulo col-12 mt-5">todos</div>
 			</div>
 			<div className="row">
-				<ul class="list-group">
-					<li class="list-group-item">An item</li>
-					<li class="list-group-item">A second item</li>
-					<li class="list-group-item">A third item</li>
-					<li class="list-group-item">A fourth item</li>
-					<li class="list-group-item">And a fifth one</li>
+				<ul className="list-group ">
+					<li className="list-group-item">
+						<input
+							type="text"
+							id="name"
+							placeholder="An item"
+							onKeyDown={(e) => {
+								if (e.key == "Enter") {
+									setItems([...items, e.target.value]);
+									e.target.value = "";
+								}
+							}}
+						/>
+					</li>
+
+					{items.map((item, idx) => {
+						return (
+							<li className="list-group-item">
+								{item}
+								{idx == items.length - 1 ? (
+									<button
+										onClick={() => {
+											items.pop();
+											setItems([...items]);
+										}}>
+										X
+									</button>
+								) : (
+									""
+								)}
+							</li>
+						);
+					})}
+					<li className="list-group-item contador">
+						{items.length} item left
+					</li>
 				</ul>
 			</div>
 		</div>
