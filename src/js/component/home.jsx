0235702708dd7f66348df171e86faf7cf1de2fb4
abicelaya <abicelaya@gmail.com>
2022-04-26 11:11:40 +0000

    añado funcion eliminar cualquier tarea

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,42 +5,48 @@ import React, { useState } from "react";
 //create your first component
 const List = () => {
 	const [items, setItems] = useState([]);
+
+	const deleteItems = (indexToDel) => {
+		let result = items.filter(
+			(tarea, filterIndex) => filterIndex !== indexToDel
+		);
+		setItems(result);
+	};
+
 	return (
 		<div className="container-fluid justify-content-center">
 			<div className="row">
 				<div className="titulo col-12 mt-5">todos</div>
 			</div>
 			<div className="row">
-				<ul className="list-group ">
+				<ul className="list-group">
 					<li className="list-group-item">
 						<input
 							type="text"
 							id="name"
 							placeholder="What's need to be done?"
 							onKeyDown={(e) => {
-								if (e.key == "Enter") {
+								if (e.key == "Enter" && e.target.value != "") {
+									/* primero añadimos a la variable de estado items lo que escribio el usuario y con ... guardamos lo que ya habia*/
 									setItems([...items, e.target.value]);
+									/*limpio el imput*/
 									e.target.value = "";
 								}
 							}}
 						/>
 					</li>
-
-					{items.map((item, idx) => {
+					{/* uso map para iterar cada uno de los items guardados en la variable de estado intems*/}
+					{items.map((tarea, idx) => {
 						return (
-							<li className="list-group-item">
-								{item}
-								{idx == items.length - 1 ? (
-									<button
-										onClick={() => {
-											items.pop();
-											setItems([...items]);
-										}}>
-										X
-									</button>
-								) : (
-									""
-								)}
+							<li className="list-group-item" key={idx}>
+								{tarea}
+
+								<button
+									onClick={() => {
+										deleteItems(idx);
+									}}>
+									X
+								</button>
 							</li>
 						);
 					})}
